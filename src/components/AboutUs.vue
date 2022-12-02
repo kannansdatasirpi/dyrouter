334e12c3d0df1a5ba795092115772b4af41218c7
kannan <kannans@datasirpi.com>
2022-12-02 13:06:52 +0530

    dynamiccomponent

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="about">
+    <p id="cont">AboutUs Content</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutUs',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AboutUs from '../components/AboutUs.vue'
+import HomePage from '../components/HomePage.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomePage
   },
   {
     path: '/about',
     name: 'about',
+    component: AboutUs
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    //component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),  // in this history syntax is get the base URL like(http://localhost:8080/), then this url bind the routes. this routes assing the url in prefix of the component
   routes
 })
 
